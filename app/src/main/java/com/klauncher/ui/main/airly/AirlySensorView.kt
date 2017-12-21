a082ba063fa28a34a97ca6a10b43ac836a1790d0
darek <darek@cosmose.co>
2017-12-21 11:00:00 +0100

    Fixes for map

--- app/src/main/java/com/klauncher/ui/main/airly/AirlySensorView.kt
+++ app/src/main/java/com/klauncher/ui/main/airly/AirlySensorView.kt
@@ -10,6 +10,12 @@ import com.klauncher.model.MapSensor
 
 class AirlySensorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+    companion object {
+        const val DOT_SIZE = 7F
+        const val BOX_WIDTH = 120
+        const val BOX_HEIGHT = 40
+    }
+
     val paint = Paint().apply {
         isAntiAlias = true
         color = Color.WHITE
@@ -21,16 +27,13 @@ class AirlySensorView(context: Context, attrs: AttributeSet) : View(context, att
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.drawColor(Color.BLACK)
         if (mapSensor == null) return
         canvas?.let { canvas ->
             mapSensor?.let { mapSensor ->
-                canvas.drawCircle(
-                        7F,
-                        7F,
-                        14F,
-                        paint)
+                canvas.drawCircle(DOT_SIZE, DOT_SIZE, DOT_SIZE, paint)
 
-                //canvas.drawText("TEMP: ${mapSensor.sensor?.currentMeasurements?.temperature}", 70F, 24F, paint)
+                canvas.drawText("%.1fº".format(mapSensor.sensor?.currentMeasurements?.temperature), 30F, 18F, paint)
                 //canvas.drawText("PM10: ${mapSensor.sensor?.currentMeasurements?.pm10}", 70F, 48F, paint)
                 //canvas.drawText("PM2.5: ${mapSensor.sensor?.currentMeasurements?.pm25}", 70F, 72F, paint)
             }
@@ -38,6 +41,6 @@ class AirlySensorView(context: Context, attrs: AttributeSet) : View(context, att
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(240, 80)
+        setMeasuredDimension(BOX_WIDTH, BOX_HEIGHT)
     }
 }
