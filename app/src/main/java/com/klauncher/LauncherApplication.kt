9049f87be6b13c765df1d885d97627404a9240ba
Darek-Mac <darek.baranczuk@gmail.com>
2019-04-08 12:58:54 +0200

    Added stetho

--- app/src/main/java/com/klauncher/LauncherApplication.kt
+++ app/src/main/java/com/klauncher/LauncherApplication.kt
@@ -1,6 +1,7 @@
 package com.klauncher
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.klauncher.di.AirlyModule
 import com.klauncher.di.DefaultModule
 import net.time4j.android.ApplicationStarter
@@ -10,6 +11,9 @@ class LauncherApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
         startKoin(this, listOf(AirlyModule(), DefaultModule()))
         ApplicationStarter.initialize(this, true)
     }
