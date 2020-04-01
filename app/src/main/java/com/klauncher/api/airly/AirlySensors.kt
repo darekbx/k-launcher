947399e8815ee702773dd8433c5d98ab875e5548
Darek-Mac <darek.baranczuk@gmail.com>
2020-04-01 14:58:46 +0200

    Updated sensors

--- app/src/main/java/com/klauncher/api/airly/AirlySensors.kt
+++ app/src/main/java/com/klauncher/api/airly/AirlySensors.kt
@@ -7,11 +7,11 @@ class AirlySensors {
     companion object {
         val sensors = listOf(
                 MapSensor(732, "Górczewska", 231, 523),
-                MapSensor(1071, "Powstańców Śląskich", 346, 604),
-                MapSensor(2325, "Bema", 612, 642),
-                MapSensor(337, "Swietokrzyska", 747, 611),
+                MapSensor(12040, "Zeusa", 346, 604),
+                MapSensor(8880, "Bema", 612, 642),
+                MapSensor(3465, "Kaliskiego", 747, 611),
                 MapSensor(2525, "Krochmalna", 806, 534),
-                MapSensor(658, "Szpitalna", 930, 572)
+                MapSensor(9064, "Centrum", 930, 572)
         )
     }
 }
